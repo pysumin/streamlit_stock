69a39230a3fdf81c8a5e02430d5127f98716aa18
pysumin <josumin@gmail.com>
2023-10-25 22:53:09 +0900

    pakage import test

--- new_stock.py
+++ new_stock.py
@@ -1,6 +1,9 @@
+from pykrx import stock
+from datetime import datetime, timedelta
 import pandas as pd
 import numpy as np
 import streamlit as st
-
+import matplotlib.pyplot as plt
+import time
 
 st.write('hello')
