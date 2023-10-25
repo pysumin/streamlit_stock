20cbb6c40be048ae6107712067313275c5cf9ee7
pysumin <josumin@gmail.com>
2023-10-25 23:02:12 +0900

    Update new_stock.py

--- new_stock.py
+++ new_stock.py
@@ -1,9 +1,10 @@
-# import pykrx
+
 # from pykrx import stock
 from datetime import datetime, timedelta
 import pandas as pd
 import numpy as np
 import streamlit as st
+import pykrx
 # import matplotlib as plt
 import time
 
